dae6ab67abc39734afdb671b8e6efb54bf4b4b0a
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-10-04 07:00:34 +0300

    Add vue-i18n via cdn.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const messages = {
+  en: {
+    menu: {
+      about: 'About'
+    }
+  },
+  ua: {
+    menu: {
+      about: 'Про мене'
+    }
+  }
+};
+
+const i18n = VueI18n.createI18n({
+  locale: 'en',
+  fallbackLocale: 'ua',
+  messages,
+});
+
 const About = { template: '<h2>About Page</h2>' };
 const Tools = { template: '<h2>Tools Page</h2>' };
 const Projects = { template: '<h2>Projects</h2>' };
@@ -41,7 +60,7 @@ app.component('my-header', {
                 <button class="menu__btn"></button>
                 
                 <div class="menu__content">
-                  <router-link to="/"> About </router-link>
+                  <router-link to="/"> {{ $t("menu.about") }} </router-link>
                   <router-link to="/tools"> Used tools </router-link>
                   <router-link to="/projects"> Projects </router-link>
                   
@@ -53,4 +72,5 @@ app.component('my-header', {
 });
 
 app.use(router);
+app.use(i18n);
 app.mount('#app');
